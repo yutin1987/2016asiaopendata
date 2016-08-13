64eaf66d92e482bc4871a9c2c8bc75a8ae76152e
YuTin Liu <yuting1987@gmail.com>
2016-08-13 14:12:05 +0800

    feat(qna): update taipei 1999

---
+++ parser/1999taipei-merge.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const data = [].concat(
+  require('./1999taipei-data1.json'),
+  require('./1999taipei-data2.json'),
+  require('./1999taipei-data3.json')
+);
+
+fs.writeFileSync(`./taipei1999.json`, JSON.stringify(data));
