788cab12964e6c0ba16c9c50d3c23249f46911dc
YuTin Liu <yuting1987@gmail.com>
2016-08-13 15:48:31 +0800

    fix(feeback): reply

--- app.js
+++ app.js
@@ -254,8 +254,6 @@ function receivedMessage(event) {
     return;
   }
 
-  console.log(message);
-
   if (messageText) {
 
     // If we receive a text message, check to see if it matches any special
@@ -369,7 +367,7 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to
   // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  // sendTextMessage(senderID, "Postback called");
 }
 
 /*
