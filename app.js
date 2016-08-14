510dab251c03bd876d6ad9e275b5b4709a562f3f
YuTin Liu <yuting1987@gmail.com>
2016-08-14 13:14:34 +0800

    fix(ans): max size

--- app.js
+++ app.js
@@ -720,7 +720,7 @@ function sendConsultant(recipientId) {
           type: "template",
           payload: {
             template_type: "button",
-            text: message.substr(0, 200) + message.length > 200 ? '...' : '',
+            text: message.substr(0, 100) + message.length > 100 ? '...' : '',
             buttons: buttons,
           }
         }
