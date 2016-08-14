54618443439cd6596be308055b6157db9bb53dac
YuTin Liu <yuting1987@gmail.com>
2016-08-14 13:37:49 +0800

    fix(keyword):

--- app.js
+++ app.js
@@ -325,7 +325,7 @@ function receivedMessage(event) {
         sendAccountLinking(senderID);
 
       default:
-        sendReply(senderID, _.trim(messageText));
+        sendReply(senderID, _.trim(_.replace(messageText, pleonasm, ' ')));
     }
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
